be2ad7e0f701ac968a5d219212ab7631a80fa55c
Rachel Davey <rachel-davey@libfstudy.ac.uk>
2025-05-05 07:11:05 +0100

    Enhance meme fetching functionality and add category routes; update index and random meme templates for better layout and navigation

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ import json
 app = Flask(__name__)
 
 # This function fetches a random meme from the meme-api
-def get_meme():
-    url = "https://meme-api.com/gimme"
+def get_meme(category=None):
+    if category:
+        url = f"https://meme-api.com/gimme/{category}"
+    else:
+        url = "https://meme-api.com/gimme/"
+
     response = requests.get(url).json()
     meme_large = response["url"]
     subreddit = response["subreddit"]
@@ -15,9 +19,32 @@ def get_meme():
 
 # This route fetches a random meme and displays it
 @app.route("/randommeme/")
-def random_meme():
-    meme_pic, subreddit = get_meme()
+def random_meme(category=None):
+    meme_pic, subreddit = get_meme(category=None)
     return render_template("random_meme.html", meme_pic=meme_pic, subreddit=subreddit)
+
+# Routes for specific categories
+@app.route("/politicalmemes/")
+def political_memes():
+    meme_pic, subreddit = get_meme("politicalmemes")
+    return render_template("random_meme.html", meme_pic=meme_pic, subreddit=subreddit)
+
+@app.route("/cryptomemes/")
+def crypto_memes():
+    meme_pic, subreddit = get_meme("cryptomemes")
+    return render_template("random_meme.html", meme_pic=meme_pic, subreddit=subreddit)
+
+@app.route("/catmemes/")
+def cat_memes():
+    meme_pic, subreddit = get_meme("catmemes")
+    return render_template("random_meme.html", meme_pic=meme_pic, subreddit=subreddit)
+
+@app.route("/dogmemes/")
+def dog_memes():
+    meme_pic, subreddit = get_meme("dogmemes")
+    return render_template("random_meme.html", meme_pic=meme_pic, subreddit=subreddit)
+
+
 
 
 @app.route("/")
