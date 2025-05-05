bf3462f8a4ae92bf767404ec74e27c67fb60b079
Rachel Davey <rachel-davey@libfstudy.ac.uk>
2025-05-05 04:32:54 +0100

    Add settings for Python auto-import and update todo list with meme categories

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import json
 
 app = Flask(__name__)
 
+# This function fetches a random meme from the meme-api
 def get_meme():
     url = "https://meme-api.com/gimme"
     response = requests.get(url).json()
@@ -11,11 +12,14 @@ def get_meme():
     subreddit = response["subreddit"]
     return meme_large, subreddit
 
-@app.route("/")
-def index():
-    return render_template("index.html")
 
+# This route fetches a random meme and displays it
 @app.route("/randommeme/")
 def random_meme():
     meme_pic, subreddit = get_meme()
     return render_template("random_meme.html", meme_pic=meme_pic, subreddit=subreddit)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
